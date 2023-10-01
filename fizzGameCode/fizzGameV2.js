e9deb80cb263f85fde26ec4973e34e700e58bb27
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-10-01 12:11:56 -0400

    Feat : Add pseudo-code of the project

--- fizzGameCode/fizzGameV2.js
+++ fizzGameCode/fizzGameV2.js
@@ -1,1 +1,24 @@
 //New updated version of Fizz game///
+
+//Pseudo-code//
+// 1.Add event listener to start button//
+//  2.When start button push, show personalisez input message//
+    //2.1 Code alert message //
+    //2.1 Ask user to enter a number of turn to play and a number of turn.
+        //(create node + styling)
+        //2.1.1 : Input mesage =  DIv --> 2 TextInput --> h2 : + h2 //
+        //2.1.2 : Get back the input enter in the form
+    //2.2 : Loop ask user next number (...)
+        // 2.2 condition... fizzGame, buzzz, buzzfizz
+    //2.3: Throw GAME OVER -- Replace text with Game OVER body text OR personalised alert
+    // 2.4 Throw WIN -- Replace text with Game OVER body text OR personalised alert
+    //Add style.
+//3 : Extra : 
+    //Add timer 
+    //Add level of difficulty//
+
+//------------------------FUNCTION DEFINITION----------------------/////
+
+//Event listener for the Start button//
+
+    
