28e06a9a90c32edd4b388e79f4da66e19ee23ff6
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-10-02 08:28:51 -0400

    Feat : Add loop for each turn

--- fizzGameCode/fizzGameV2.js
+++ fizzGameCode/fizzGameV2.js
@@ -1,4 +1,4 @@
-import {AddNodeToDOM,styleNode} from "./specialisedFunction"
+import {AddNodeToDOM,styleNode} from "./specialisedFunction.js"
 
 //New updated version of Fizz game///
 
@@ -18,21 +18,32 @@ import {AddNodeToDOM,styleNode} from "./specialisedFunction"
 //3 : Extra : 
     //Add timer 
     //Add level of difficulty//
+    //Add life//
+
+    //https://www.altcademy.com/blog/how-to-get-user-input-in-javascript///
+    //https://www.google.com/search?q=Ask+input+to+the+user+J+S&sca_esv=569877865&ei=n7UZZYXMI4ep5NoPlPq02Aw&ved=0ahUKEwjFl-ncuNWBAxWHFFkFHRQ9DcsQ4dUDCBA&uact=5&oq=Ask+input+to+the+user+J+S&gs_lp=Egxnd3Mtd2l6LXNlcnAiGUFzayBpbnB1dCB0byB0aGUgdXNlciBKIFMyBRAhGKABMgUQIRigATIFECEYoAEyCBAhGBYYHhgdMggQIRgWGB4YHTIIECEYFhgeGB0yCBAhGBYYHhgdMggQIRgWGB4YHTIIECEYFhgeGB0yCBAhGBYYHhgdSLosUABYjCtwA3gAkAEAmAF7oAGmEqoBBDIzLjS4AQPIAQD4AQGoAgrCAhYQABgDGI8BGOUCGOoCGLQCGIwD2AEBwgIWEC4YAxiPARjlAhjqAhi0AhiMA9gBAcICCBAAGIoFGJECwgIREC4YgwEYxwEYsQMY0QMYgATCAg4QLhiABBixAxjHARjRA8ICCBAAGIAEGLEDwgILEAAYgAQYsQMYgwHCAhEQLhiABBixAxiDARjHARjRA8ICCxAuGIAEGMcBGNEDwgILEAAYigUYsQMYgwHCAgcQABiKBRhDwgINEC4YigUYxwEY0QMYQ8ICDRAuGIoFGLEDGIMBGEPCAgsQLhiKBRixAxiDAcICCBAuGIAEGLEDwgILEC4YgAQYsQMYgwHCAhAQLhjHARixAxjRAxiKBRhDwgIREC4YigUYsQMYgwEYxwEYrwHCAgkQABiKBRgKGEPCAhAQLhiKBRixAxjHARjRAxhDwgIFEAAYgATCAgUQLhiABMICHBAuGIoFGMcBGNEDGEMYlwUY3AQY3gQY4ATYAQLCAgsQABgWGB4Y8QQYCsICBhAAGBYYHsICBxAAGA0YgATCAgYQABgeGA3CAggQABiKBRiGA8ICCBAAGBYYHhgPwgIHECEYoAEYCsICBBAhGBXCAgoQIRgWGB4YDxgd4gMEGAAgQYgGAboGBAgBGAq6BgYIAhABGBQ&sclient=gws-wiz-serp
 
 //------------------------FUNCTION DEFINITION----------------------/////
 
 const launchGame = () => {
-    
+
+    //Get the number of turn indicated by the user in the input//
+    let numberOfTurnToPlay = parseInt(numberOfTurn.value); 
+
+    //loop for each turn of the game//
+    for (let index = 0; index < numberOfTurnToPlay; index++) {
+        
+    }
 }
 
-showInputWindow = {
+// showInputWindow = {
 
-}
+// }
 
 
 
 
 //------------------------END OF FUNCTION DEFINITION----------------/////
 //Event listener for the Start button//
-startButton.AddEventListener("click",(launchGame))
+startButton.addEventListener("click",(launchGame));
     
